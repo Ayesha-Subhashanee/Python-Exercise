303536b6c82b9c371dc078c1619e45c9e0971f57
ayesh <ayeshasubha700@gmail.com>
2025-08-21 04:20:53 +0900

    practice

--- Ayesha.py
+++ Ayesha.py
@@ -1,5 +1,21 @@
 print("Hello, Ayesha")
-print("hi everyone")
+print("Hi Everyone")
+print("This is my first project")
+
+name = "Ayesha"
+age = 34
+print("My name is ", name)
+print("I am", age, "years old")
+
+x = 5
+y = 10
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:",x / y)
+
+
+
 
 
 
