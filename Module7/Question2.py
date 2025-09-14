bf2090f250e047f34913878af573134be71373ec
ayesh <ayeshasubha700@gmail.com>
2025-09-15 02:46:19 +0900

    Module7Answers

---
+++ Module7/Question2.py
@@ -0,0 +1,18 @@
+# Use a set to store unique names
+names = set()
+
+while True:
+    name = input("Enter a name (empty string to stop): ")
+
+    if name == "":
+        break  # stop when empty string entered
+
+    if name in names:
+        print("Existing name")
+    else:
+        print("New name")
+        names.add(name)
+
+print("\nAll entered names:")
+for n in names:
+    print(n)
---
+++ MySQLpractice.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='gamedb',
+         user='root',
+         password='907360270@aks',
+         autocommit=True
+         )
