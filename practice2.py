61bee5ba9080cbb09e70e8d35b1e03c061232a64
ayesh <ayeshasubha700@gmail.com>
2025-11-18 15:33:07 +0900

    devide by task

--- practice2.py
+++ practice2.py
@@ -1,7 +1,18 @@
-def greet():
-    print("Hello!")
-    return
+class Spacecraft:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
-print("A new day starts with a greeting.")
-greet()
-print("Now we can move to other business.")
+    def display_info(self):
+        print(f"Brand: {self.brand}, Model: {self.model}")
+
+
+class Shuttle(Spacecraft):
+    def __init__(self, brand, model, engine):
+        super().__init__(brand, model)
+        self.engine = engine
+
+    def display_info(self):
+        super().display_info()
+        print(f"Brand: {self.brand}, Model: {self.model}")
+        print(f"Number of engines: {self.engine}")
